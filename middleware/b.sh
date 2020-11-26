82de8d1669a72ea12880f324dc3bc4b0821b6939
maicol <mmoarias4@gmail.com>
2020-11-26 18:12:41 -0500

    update

--- middleware/b.sh
+++ middleware/b.sh
@@ -1,2 +1,30 @@
-s=`curl -o /dev/null -s -w %{size_download}  http://localhost:8000/`
-echo "dasdsadsadsadsa....."
+#!/bin/bash
+while :
+do
+	
+    s1=`curl -o /dev/null -s -w %{size_download}  $1`
+    if [ $s1 !=  0 ];
+    then 
+        info="`date` SERVER 1: SERVICE RUNNING."
+        `echo $info >> logs/history.log`
+        `echo $info > logs/latest.log`
+    else 
+        info="`date` SERVER 1: SERVICE DOWN."
+        `echo $info >> logs/history.log`
+        `echo $info > logs/latest.log`
+    fi
+
+    s2=`curl -o /dev/null -s -w %{size_download}  $2`
+    if [ $s2 !=  0 ];
+    then 
+        info="`date` SERVER 2: SERVICE RUNNING."
+        `echo $info >> logs/history.log`
+        `echo $info >> logs/latest.log`
+    else 
+        info="`date` SERVER 2: SERVICE DOWN."
+        `echo $info >> logs/history.log`
+        `echo $info >> logs/latest.log`
+    fi
+
+    sleep 2
+done
--- middleware/index.js
+++ middleware/index.js
@@ -1,76 +1,32 @@
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
-var exec = require('child_process').exec;
-const shell = require('shelljs');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+
+const pathFile = 'logs/latest.log';
+
 var serversUrls = ["hola", "mundo"];
-var pathsFiles = ['Modifieds.txt', 'LastUpdate.txt'];
 
 
 app.use(cors());
 app.use(express.json());
 
+// read file and send response
 app.get('/', async (req, res) => {
-    // read file and send response
-
-
-    /*fs.readFile(pathsFiles[1], (err, data) => {
-        res.send(err ? 'error' : data.toString());
-    });*/
+    fs.readFile(pathFile, (err, data)=>{
+        res.send(data.toString());
+    });
 });
 
 app.post('/', async (req, res) => {
+    var id = req.body.id;
     // reiniciar ese servidor
 });
 
-
-/*child = exec('sh bash.sh http://localhost:8000/ http://localhost:8001/',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-    });*/
-async function sh(cmd) {
-    return new Promise(function (resolve, reject) {
-        exec(cmd, (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ stdout, stderr });
-            }
-        });
-    });
-}
-
-
-// ping to servers and write files
-async function requestServers() {
-    while (true) {
-        var result = '';
-        /*let { stdout } = await sh('sh b.sh');
-        console.log(stdout);
-        for (let line of stdout.split('\n')) {
-            console.log(line);
-        }*/
-
-        shell.exec('sh b.sh > out.txt');
-/*
-        fs.appendFile(pathsFiles[0], result, (err) => {
-            if (err) console.log('error write file');
-        });
-        fs.writeFile(pathsFiles[1], result, (err) => {
-            if (err) console.log('error write file');
-        });*/
-        await sleep(2000);
-    }
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 app.listen(port, () => {
     console.log(`middleware running on port ${port}`);
-    requestServers();
 });
